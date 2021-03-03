0528840a4fdd1026a8d138420fdeca0065ef2b0f
AdastormVproI7 <jonah.watts@gmail.com>
2021-03-03 16:31:30 +0000

    Updated the file input

--- Main.java
+++ Main.java
@@ -1,25 +1,43 @@
-import javax.swing.JOptionPane;
+//Jonah Watts, 2021
 import java.io.*;
+import java.util.*;
+
 class Main {
-  String masterString;
+  
+  //Data
+  private String masterString;
+  scanner s = new scanner(System.in);
+
   public static void main(String[] args) {
-    JOptionPane.showConfirmDialog(null, "hola");
-    try {
+    masterString= new String("");
+
+
+    //First get the class name for the file
+    System.out.println("Please Enter the name of your class");
+
+
+    try 
+    {
       File myObj = new File("filename.txt");
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
       } else {
         System.out.println("File already exists.");
       }
-    } catch (IOException e) {
+    } 
+    catch (IOException e) 
+    {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+
+    
   }
 
- addFunction(String Name, String preCon, String postCon)
+/*
+ private void addFunction(String Name, String preCon, String postCon)
  {
-   masterString = masterString + name +"\n  PRECONDITION:" + 
+   masterString = "" + masterString +"  " +name +"\n  PRECONDITION:" + precon + "\n  POSTCONDITION:"+postCon + "\n";
  }
-  
+  */
 }
